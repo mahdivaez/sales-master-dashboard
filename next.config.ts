64b33ba88a54d224ab203d0d300433d4a8e634d1
Mehdi <mahdivaeztehrani81@gmail.com>
2026-01-30 01:39:38 +0330

    update

--- next.config.ts
+++ next.config.ts
@@ -7,13 +7,6 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
-  // In Next.js 16, Turbopack is enabled by default.
-  // To silence the error when using a custom webpack config, 
-  // we must provide an empty turbopack config object.
-  // The key is 'experimental.turbopack' in Next.js 16.
-  experimental: {
-    turbopack: {}
-  }
 };
 
 export default nextConfig;
